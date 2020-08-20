9b2c690d41ef893c520e1ad76d57a237f300fbfd
Michael Berry <trismegustis@gmail.com>
2020-08-19 20:00:48 -0400

    Cleaned up and modified prompts.

--- .zsh/prompts.zsh
+++ .zsh/prompts.zsh
@@ -1,3 +1,12 @@
+# Version control info
+autoload -Uz vcs_info compinit && compinit
+
+zstyle ':vcs_info:*' enable git hg svn cvs bzr
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' get-revision true
+zstyle ':vcs_info:(git|hg|svn|cvs|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+zstyle ':vcs_info:*' formats "%{$fg[magenta]%}(%{$fg[green]%}%s%{$fg[magenta]%}) %{$fg[magenta]%}%c%{$fg[green]%}%u%{$fg[yellow]%} %b%{$reset_color%}:%{$fg[cyan]%}%r%{$reset_color%}"
+
 # color username
 username() {
     color=$1
@@ -14,7 +23,26 @@ current_time() {
     echo "%*"
 }
 
-# returns 👾 if there are errors, nothing otherwise
+# returns 👾 and return code if there are errors, nothing otherwise
 return_status() {
     echo "%(?..👾 %?)"
+}
+
+# determine remote ssh host
+remote_host() {
+    if [[ -z "$SSH_CLIENT" ]]; then
+	echo ""
+    else
+	echo "%{$fg_bold[white]%}@%{$reset_color$fg[yellow]%}$(hostname -s)"
+    fi
+}
+
+vcs_info_wrapper() {
+    remote=$(remote_host)
+
+    if [[ ${vcs_info_msg_0_} ]]; then
+	echo "$(username magenta)${remote} ${vcs_info_msg_0_} $(current_time)%{$reset_color%}"
+    else
+	echo "$(username magenta)${remote} $(current_time)%{$reset_color%}"
+    fi
 }
--- .zsh/prompts/berrym-default.zsh
+++ .zsh/prompts/berrym-default.zsh
@@ -1,29 +1,8 @@
-# Version control info
-autoload -Uz vcs_info compinit && compinit
-
-zstyle ':vcs_info:*' enable git hg svn cvs bzr
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' formats "%{$fg[magenta]%}%c%{$fg[green]%}%u\
-%{$fg[magenta]%} [%{$fg[green]%}%b%{$fg[magenta]%}]\
--%{$fg[yellow]%}%s%{$reset_color%}:%{$fg[cyan]%}%r%{$reset_color%}"
-
 # Prompt configuration
 load_custom_prompt() {
     setopt PROMPT_SUBST       # needed for vcs_info_msg_0_
-    # prompt without name
     PROMPT="%(?.%{$fg[cyan]%}.%{$fg[red]%})%B$(directory)%b%{$reset_color%} %% "
-
-    if [[ -z "$SSH_CLIENT" ]]; then
-	prompt_host=""
-    else
-	prompt_host=%{$fg_bold[white]%}@%{$reset_color$fg[yellow]%}$(hostname -s)
-    fi
-
-    if [[ ${vcs_info_msg_0_} ]]; then
-	RPROMPT="$(username magenta)${prompt_host} ${vcs_info_msg_0_}%{$reset_color%}"
-    else
-	RPROMPT="$(username magenta)${prompt_host} $(current_time)%{$reset_color%}"
-    fi
+    RPROMPT=$(vcs_info_wrapper)
 }
 
 # Load vcs info before each prompt
