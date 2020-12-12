d4f87a3ad2535a22cdc4ca4e255f1ca799779e94
Michael Berry <trismegustis@gmail.com>
2020-12-12 14:46:33 -0500

    Changed the way aliases are loaded.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,48 +5,48 @@
 
 print - "Detecting OS to load aliases."
 
-if [[ isLinux ]]; then
-    local ALIASFILE=${ALIASDIR}/linux-aliases.zsh
-    if [[ -r  $ALIASFILE ]]; then
-        print - "Loading aliases from $ALIASFILE"
-        . $ALIASFILE
-    else
-        print - "Unable to source $ALIASFILE"
-    fi
-elif [[ isDarwin ]]; then
-    local ALIASFILE=${ALIASDIR}/darwin-aliases.zsh
-    if [[ -r  $ALIASFILE ]]; then
-        print - "Loading aliases from $ALIASFILE"
-        . $ALIASFILE
-    else
-        print - "Unable to source $ALIASFILE"
-    fi
-elif [[ isFreeBSD ]]; then
-    local ALIASFILE=${ALIASDIR}/freebsd-aliases.zsh
-    if [[ -r  $ALIASFILE ]]; then
-        print - "Loading aliases from $ALIASFILE"
-        . $ALIASFILE
-    else
-        print - "Unable to source $ALIASFILE"
-    fi
-elif [[ isOpenBSD ]]; then
-    local ALIASFILE=${ALIASDIR}/openbsd-aliases.zsh
-    if [[ -r  $ALIASFILE ]]; then
-        print - "Loading aliases from $ALIASFILE"
-        . $ALIASFILE
-    else
-        print - "Unable to source $ALIASFILE"
-    fi
-elif [[ isNetBSD ]]; then
-    local ALIASFILE=${ALIASDIR}/netbsd-aliases.zsh
+if [[ $OSNAME == "Linux" ]]; then
+    ALIASFILE=${ALIASDIR}/linux-aliases.zsh
     if [[ -r $ALIASFILE ]]; then
         print - "Loading aliases from $ALIASFILE"
         . $ALIASFILE
     else
         print - "Unable to source $ALIASFILE"
     fi
-elif [[ isDragonFly ]]; then
-    local ALIASFILE=${ALIASDIR}/dragonfly-aliases.zsh
+elif [[ $OSNAME == "OpenBSD" ]]; then
+    ALIASFILE=${ALIASDIR}/darwin-aliases.zsh
+    if [[ -r $ALIASFILE ]]; then
+        print - "Loading aliases from $ALIASFILE"
+        . $ALIASFILE
+    else
+        print - "Unable to source $ALIASFILE"
+    fi
+elif [[ $OSNAME == "FreeBSD" ]]; then
+    ALIASFILE=${ALIASDIR}/freebsd-aliases.zsh
+    if [[ -r $ALIASFILE ]]; then
+        print - "Loading aliases from $ALIASFILE"
+        . $ALIASFILE
+    else
+        print - "Unable to source $ALIASFILE"
+    fi
+elif [[ $OSNAME == "OpenBSD" ]]; then
+    ALIASFILE=${ALIASDIR}/openbsd-aliases.zsh
+    if [[ -r $ALIASFILE ]]; then
+        print - "Loading aliases from $ALIASFILE"
+        . $ALIASFILE
+    else
+        print - "Unable to source $ALIASFILE"
+    fi
+elif [[ $OSNAME == "NetBSD" ]]; then
+    ALIASFILE=${ALIASDIR}/netbsd-aliases.zsh
+    if [[ -r $ALIASFILE ]]; then
+        print - "Loading aliases from $ALIASFILE"
+        . $ALIASFILE
+    else
+        print - "Unable to source $ALIASFILE"
+    fi
+elif [[ $OSNAME == "DragonFly" ]]; then
+    ALIASFILE=${ALIASDIR}/dragonfly-aliases.zsh
     if [[ -r $ALIASFILE ]]; then
         print - "Loading aliases from $ALIASFILE"
         . $ALIASFILE
--- .zsh/prompts/berrym-arrows.zsh
+++ .zsh/prompts/berrym-arrows.zsh
@@ -1,6 +1,6 @@
 # Prompt configuration
 load_custom_prompt() {
     setopt PROMPT_SUBST       # needed for vcs_info_msg_0_
-    PROMPT="%(?.$(directory)%{$fg[cyan]%} ➜.$(directory) %{$fg[red]%} $(return_status true) ➜)%{$reset_color%} "
+    PROMPT="%(?.$(directory)%{$fg[cyan]%} ➜.$(directory) %{$fg[red]%} $(return_status true) ➜) %{$reset_color%} "
     RPROMPT=$(vcs_info_wrapper)
 }
