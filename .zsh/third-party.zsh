3306a089ed3892db1f5be45e9fb126a6864b6f24
Michael Berry <trismegustis@gmail.com>
2023-12-10 03:16:04 -0500

    Fixed loading third party zsh packages.

--- .zsh/third-party.zsh
+++ .zsh/third-party.zsh
@@ -3,8 +3,8 @@
 # (c) 2023 Michael Berry <trismegustis@gmail.com>
 
 local -a third_party_scripts=(
-    $ZSH_THIRD_PARTY_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
-    $ZSH_THIRD_PARTY_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    $THIRD_PARTY_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+    $THIRD_PARTY_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 )
 
 for f in $third_party_scripts; do
